3038eed93607ffb5e38d9c9438247e2c071c1506
David-Escalona <120495685+David-Escalona@users.noreply.github.com>
2025-06-03 16:36:50 +0000

    Ejercicio 3 - Implementación del menu de navegación

---
+++ my-app/src/app/movieexplorer/page.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import React from "react";
+
+const MovieExplorer = () => {
+  return (
+    <main className="p-4">
+      <h1>MovieExplorer</h1>
+      <p>Aquí irá el contenido de la vista MovieExplorer.</p>
+    </main>
+  );
+};
+
+export default MovieExplorer;
